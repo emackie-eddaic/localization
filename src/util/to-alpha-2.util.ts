8d84386b43ca81ee1fd8fa0a7a6acca82e10f652
Eric Mackie <eric.mackie@essolutions.ca>
2024-09-17 09:36:47 -0400

    fix(util): `toAlpha` function calling itself

--- src/util/to-alpha-2.util.ts
+++ src/util/to-alpha-2.util.ts
@@ -1,7 +1,8 @@
+import { toAlpha2 as toAlpha2Package } from 'i18n-iso-countries';
 import { Alpha2Code } from '../type';
 
 export function toAlpha2(alpha3: string): Alpha2Code {
-  const code = toAlpha2(alpha3);
+  const code = toAlpha2Package(alpha3);
   if (!code) {
     throw new Error(`${alpha3} could not be converted to Alpha2 code`);
   } else {
--- src/util/to-alpha-3.util.ts
+++ src/util/to-alpha-3.util.ts
@@ -1,7 +1,8 @@
+import { toAlpha3 as toAlpha3Package } from 'i18n-iso-countries';
 import { Alpha3Code } from '../type';
 
 export function toAlpha3(alpha2: string): Alpha3Code {
-  const code = toAlpha3(alpha2);
+  const code = toAlpha3Package(alpha2);
   if (!code) {
     throw new Error(`${alpha2} could not be converted to Alpha3 code`);
   } else {
